7f84262989d3e411c649111be91d7cc03244c291
Jayu-29 <jaibaradia29@gmail.com>
2026-03-18 04:32:26 +0000

    Challenge 01 Completed

--- src/01-aadhaar-masker.js
+++ src/01-aadhaar-masker.js
@@ -29,4 +29,8 @@
  */
 export function maskAadhaar(aadhaarNumber) {
   // Your code here
+  if(typeof aadhaarNumber !== "string" || aadhaarNumber.length !== 12 || !aadhaarNumber.match(/^[0-9]+$/)){
+    return "INVALID";
+  }
+  return "XXXX-XXXX-" + aadhaarNumber.slice(8);
 }
